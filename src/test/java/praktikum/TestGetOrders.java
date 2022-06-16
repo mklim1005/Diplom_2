6e7041af9010017e2ae72f52d4ae2742d930040f
Marina Klimova <m.klim1005@gmail.com>
2022-06-16 15:15:49 +0300

    refactor TestGetOrders

--- src/test/java/praktikum/TestGetOrders.java
+++ src/test/java/praktikum/TestGetOrders.java
@@ -9,9 +9,9 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
+import static helpers.AuthHelper.*;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
-
 
 public class TestGetOrders {
     public String email = RandomStringUtils.randomAlphabetic(10) + "@mailinator.com";
@@ -24,65 +24,35 @@ public class TestGetOrders {
     public void setUp() {
         RestAssured.baseURI = "https://stellarburgers.nomoreparties.site";
         user = new User(email, password, name);
-        //register user
-        Response response = given()
-                .header("Content-type", "application/json")
-                .and()
-                .body(user)
-                .when()
-                .post("/api/auth/register");
-        response.then().assertThat().statusCode(200)
-                .and()
-                .assertThat().body("success", equalTo(true));
+        register(user).then().assertThat().statusCode(200);
     }
 
     @After
     public void deleteUser() {
-        Response responseLogin = given()
-                .header("Content-type", "application/json")
-                .and()
-                .body(user)
-                .when()
-                .post("/api/auth/login");
-        String accessToken;
-        accessToken = responseLogin.body().jsonPath().getString("accessToken");
+        String accessToken = loginAndGetToken(user);
 
         if (accessToken == null) {
             System.out.println("Skip delete user");
         } else {
-            Response responseDelete = given()
-                    .header("Authorization", accessToken)
-                    .delete("/api/auth/user");
-            responseDelete.then().assertThat().statusCode(202)
-                    .and()
-                    .assertThat().body("success", equalTo(true))
-                    .and()
-                    .assertThat().body("message", equalTo("User successfully removed"));
+            delete(accessToken).then().assertThat().statusCode(202);
         }
     }
 
     @Test
     public void getOrdersNotAuthorizedUser() {
-        Response response1 = given()
+        Response response = given()
                 .header("Content-type", "application/json")
                 .get("/api/orders");
-        response1.then().assertThat().statusCode(401)
-                .and()
+
+        response.then().assertThat().statusCode(401)
                 .assertThat().body("success", equalTo(false))
                 .assertThat().body("message", equalTo("You should be authorised"));
     }
 
     @Test
     public void getOrderAuthorizedUser() {
-        //log In User
-        Response responseLogin = given()
-                .header("Content-type", "application/json")
-                .and()
-                .body(user)
-                .when()
-                .post("/api/auth/login");
-        String accessToken;
-        accessToken = responseLogin.body().jsonPath().getString("accessToken");
+
+        String accessToken = loginAndGetToken(user);
 
         // create order
         //get all ingredients and pick 2 random
@@ -90,13 +60,12 @@ public class TestGetOrders {
                 .header("Content-type", "application/json")
                 .get("/api/ingredients");
         response.then().assertThat().statusCode(200)
-                .and()
                 .assertThat().body("success", equalTo(true));
         //create order with random ingredients
         GetIngredientsResponse getIngredientsResponse = response.body().as(GetIngredientsResponse.class);
         int size = getIngredientsResponse.getData().size();
-        int random1 = (int) (Math.random() * (size - 0)) + 0;
-        int random2 = (int) (Math.random() * (size - 0)) + 0;
+        int random1 = (int) (Math.random() * (size - 0));
+        int random2 = (int) (Math.random() * (size - 0));
         String firstRandomIngredient = getIngredientsResponse.getData().get(random1).get_id();
         String secondRandomIngredient = getIngredientsResponse.getData().get(random2).get_id();
         ArrayList<String> ingredients = new ArrayList<>();
@@ -117,7 +86,9 @@ public class TestGetOrders {
                 .header("Content-type", "application/json")
                 .header("Authorization", accessToken)
                 .get("/api/orders");
-        response1.then().assertThat().statusCode(200)
+
+        response1.then()
+                .assertThat().statusCode(200)
                 .assertThat().body("success", equalTo(true))
                 .assertThat().body("orders[0].status", equalTo("done"))
                 .assertThat().body("orders[0].ingredients[0]", equalTo(firstRandomIngredient))
