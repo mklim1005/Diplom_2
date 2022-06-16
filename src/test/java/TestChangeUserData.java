5922744bd61e77573bbcdab69b735230100bf390
Marina Klimova <m.klim1005@gmail.com>
2022-06-16 13:58:36 +0300

    Refactor TestChangeUserData

--- src/test/java/TestChangeUserData.java
+++ src/test/java/TestChangeUserData.java
@@ -5,7 +5,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -19,107 +18,84 @@ public class TestChangeUserData {
     public void setUp() {
         user = new User(email, password, name);
         RestAssured.baseURI = "https://stellarburgers.nomoreparties.site";
-        Response response = given()
+        given()
                 .header("Content-type", "application/json")
-                .and()
                 .body(user)
-                .when()
-                .post("/api/auth/register");
-        response.then().assertThat().statusCode(200)
-                .and()
-                .assertThat().body("success", equalTo(true));
+                .post("/api/auth/register")
+                .then().assertThat().statusCode(200);
     }
 
     @After
     public void deleteUser() {
-        Response responseLogin = given()
-                .header("Content-type", "application/json")
-                .and()
-                .body(user)
-                .when()
-                .post("/api/auth/login");
-        String accessToken;
-        accessToken = responseLogin.body().jsonPath().getString("accessToken");
-
+        String accessToken = loginAndGetToken(user);
         if (accessToken == null) {
             System.out.println("Skip delete user");
         } else {
-            Response responseDelete = given()
+            given()
                     .header("Authorization", accessToken)
-                    .delete("/api/auth/user");
-            responseDelete.then().assertThat().statusCode(202)
-                    .and()
-                    .assertThat().body("success", equalTo(true))
-                    .and()
-                    .assertThat().body("message", equalTo("User successfully removed"));
+                    .delete("/api/auth/user")
+                    .then().assertThat().statusCode(202);
         }
+    }
+
+    private Response login(User user) {
+        return given()
+                .header("Content-type", "application/json")
+                .body(user)
+                .post("/api/auth/login");
+    }
+
+    private String loginAndGetToken(User user) {
+        Response response = login(user);
+        return response.body().jsonPath().getString("accessToken");
     }
 
     @Test
     public void testChangeUserDataNameAuthorized() {
-        Response responseLogin = given()
-                .header("Content-type", "application/json")
-                .and()
-                .body(user)
-                .when()
-                .post("/api/auth/login");
-        String accessToken;
-        accessToken = responseLogin.body().jsonPath().getString("accessToken");
-
+        String accessToken = loginAndGetToken(user);
         String newName = RandomStringUtils.randomAlphabetic(10);
+
         Response response = given()
                 .header("Authorization", accessToken)
                 .header("Content-type", "application/json")
-                .and()
                 .body("{\"name\":\"" + newName + "\"}")
-                .when()
                 .patch("/api/auth/user");
-        response.then().assertThat().statusCode(200)
-                .and()
+
+        response.then()
+                .assertThat().statusCode(200)
                 .assertThat().body("success", equalTo(true))
-                .and()
                 .assertThat().body("user.name", equalTo(newName));
     }
 
     @Test
     public void testChangeUserDataEmailAuthorized() {
-        Response responseLogin = given()
-                .header("Content-type", "application/json")
-                .and()
-                .body(user)
-                .when()
-                .post("/api/auth/login");
-        String accessToken;
-        accessToken = responseLogin.body().jsonPath().getString("accessToken");
-
+        String accessToken = loginAndGetToken(user);
         String newEmail = RandomStringUtils.randomAlphabetic(10) + "@mailinator.com";
+
         Response response = given()
                 .header("Authorization", accessToken)
                 .header("Content-type", "application/json")
-                .and()
                 .body("{\"email\":\"" + newEmail + "\"}")
-                .when()
                 .patch("/api/auth/user");
-        response.then().assertThat().statusCode(200)
-                .and()
+
+        response.then()
+                .assertThat().statusCode(200)
                 .assertThat().body("success", equalTo(true))
-                .and()
                 .assertThat().body("user.email", equalTo(newEmail.toLowerCase()));
     }
 
     @Test
     public void testChangeDataNotAuthorized() {
         String newName = RandomStringUtils.randomAlphabetic(10);
+
         Response response = given()
                 .header("Content-type", "application/json")
-                .and()
                 .body("{\"name\":\"" + newName + "\"}")
-                .when()
                 .patch("/api/auth/user");
-        response.then().assertThat().statusCode(401)
-                .and()
+
+        response.then()
+                .assertThat().statusCode(401)
                 .assertThat().body("success", equalTo(false))
-                .and()
                 .assertThat().body("message", equalTo("You should be authorised"));
     }
 }
