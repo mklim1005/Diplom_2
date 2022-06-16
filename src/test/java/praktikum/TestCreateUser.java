f0e39d98f6d4f369438e168f384b2570bc7e0ba0
Marina Klimova <m.klim1005@gmail.com>
2022-06-16 14:36:12 +0300

    refactored TestCreateUser

--- src/test/java/praktikum/TestCreateUser.java
+++ src/test/java/praktikum/TestCreateUser.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static helpers.AuthHelper.loginAndGetToken;
 
 public class TestCreateUser {
     public String email = RandomStringUtils.randomAlphabetic(10) + "@mailinator.com";
@@ -24,26 +25,15 @@ public class TestCreateUser {
 
     @After
     public void deleteUser() {
-        Response responseLogin = given()
-                .header("Content-type", "application/json")
-                .and()
-                .body(user)
-                .when()
-                .post("/api/auth/login");
-        String accessToken;
-        accessToken = responseLogin.body().jsonPath().getString("accessToken");
+        String accessToken = loginAndGetToken(user);
 
         if (accessToken == null) {
             System.out.println("Skip delete user");
         } else {
-            Response responseDelete = given()
+            given()
                     .header("Authorization", accessToken)
-                    .delete("/api/auth/user");
-            responseDelete.then().assertThat().statusCode(202)
-                    .and()
-                    .assertThat().body("success", equalTo(true))
-                    .and()
-                    .assertThat().body("message", equalTo("User successfully removed"));
+                    .delete("/api/auth/user")
+                    .then().assertThat().statusCode(202);
         }
     }
 
@@ -51,37 +41,29 @@ public class TestCreateUser {
     public void testCreateUniqueUser() {
         Response response = given()
                 .header("Content-type", "application/json")
-                .and()
                 .body(user)
-                .when()
                 .post("/api/auth/register");
-        response.then().assertThat().statusCode(200)
-                .and()
+
+        response.then()
+                .assertThat().statusCode(200)
                 .assertThat().body("success", equalTo(true));
     }
 
     @Test
     public void testCreateUserThatAlreadyExists() {
-        Response successResponse = given()
+        given()
                 .header("Content-type", "application/json")
-                .and()
                 .body(user)
-                .when()
                 .post("/api/auth/register");
-        successResponse.then().assertThat().statusCode(200)
-                .and()
-                .assertThat().body("success", equalTo(true));
 
         Response response = given()
                 .header("Content-type", "application/json")
-                .and()
                 .body(user)
-                .when()
                 .post("/api/auth/register");
-        response.then().assertThat().statusCode(403)
-                .and()
+
+        response.then()
+                .assertThat().statusCode(403)
                 .assertThat().body("success", equalTo(false))
-                .and()
                 .assertThat().body("message", equalTo("User already exists"));
     }
 
@@ -90,14 +72,12 @@ public class TestCreateUser {
         user.setEmail("");
         Response response = given()
                 .header("Content-type", "application/json")
-                .and()
                 .body(user)
-                .when()
                 .post("/api/auth/register");
-        response.then().assertThat().statusCode(403)
-                .and()
+
+        response.then()
+                .assertThat().statusCode(403)
                 .assertThat().body("success", equalTo(false))
-                .and()
                 .assertThat().body("message", equalTo("Email, password and name are required fields"));
     }
 
@@ -106,14 +86,12 @@ public class TestCreateUser {
         user.setPassword("");
         Response response = given()
                 .header("Content-type", "application/json")
-                .and()
                 .body(user)
-                .when()
                 .post("/api/auth/register");
-        response.then().assertThat().statusCode(403)
-                .and()
+
+        response.then()
+                .assertThat().statusCode(403)
                 .assertThat().body("success", equalTo(false))
-                .and()
                 .assertThat().body("message", equalTo("Email, password and name are required fields"));
     }
 
@@ -122,14 +100,12 @@ public class TestCreateUser {
         user.setName("");
         Response response = given()
                 .header("Content-type", "application/json")
-                .and()
                 .body(user)
-                .when()
                 .post("/api/auth/register");
-        response.then().assertThat().statusCode(403)
-                .and()
+
+        response.then()
+                .assertThat().statusCode(403)
                 .assertThat().body("success", equalTo(false))
-                .and()
                 .assertThat().body("message", equalTo("Email, password and name are required fields"));
     }
 }
